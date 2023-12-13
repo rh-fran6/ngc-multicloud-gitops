ccec08a9b0839ffb77ed1d62677b34ee0987c93e
rh-fran6 <francis.anyaegbu@francisanyaegbu.local>
2023-12-12 22:58:38 -0600

    updated rhacs central url name on the jobs

---
+++ login.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+CLUSTER=dd-aro
+RESOURCEGROUP=aro-main
+val=$(az aro list-credentials --name $CLUSTER --resource-group $RESOURCEGROUP )
+api=$(az aro show -g $RESOURCEGROUP -n $CLUSTER --query apiserverProfile.url -o tsv)
+
+username=$(echo $val | jq -r ".kubeadminUsername")
+password=$(echo $val | jq -r ".kubeadminPassword")
+
+echo oc login -u $username -p $password $api > /opt/homebrew/bin/aro-login
+chmod +x /opt/homebrew/bin/aro-login
