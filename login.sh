4551df2f319d3c33f4d855960792190d264faa82
rh-fran6 <francis.anyaegbu@francisanyaegbu.local>
2023-12-18 14:28:00 -0600

    changes operatorconfig namespace

--- login.sh
+++ login.sh
@@ -11,38 +11,6 @@ echo oc login -u $username -p $password $api > /opt/homebrew/bin/aro-login
 chmod +x /opt/homebrew/bin/aro-login
 
 
-cat << EOF | kubectl apply -f -
-apiVersion: external-secrets.io/v1alpha1
-kind: SecretStore
-metadata:
-  name: azure-backend
-  namespace: ekho-external-secrets
-spec:
-  provider:
-    azurekv:
-      authType: ManagedIdentity
-      environmentType: PublicCloud ##USGovernmentCloud
-      tenantId: 64dc69e4-d083-49fc-9569-ebece1dd1408
-      identityId: 469f13ac-a9f4-417a-bc92-0eceaed61eca
-      vaultUrl: "https://dd-aro-cluster-akv-demo.vault.azure.net/"
----
-apiVersion: external-secrets.io/v1alpha1
-kind: ExternalSecret
-metadata:
-  name: azure-example
-  namespace: ekho-external-secrets
-spec:
-  refreshInterval: 1h
-  secretStoreRef:
-    kind: SecretStore
-    name: azure-backend
-  target:
-    name: azure-secret
-  data:
-  - secretKey: foobar
-    remoteRef:
-      key: example-externalsecret-key
-EOF
 
 
 
